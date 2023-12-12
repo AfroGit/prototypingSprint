09d1a2dded1d76a30539a5c0c31f0bd1d9d84022
Maurice Mulili <c0decampzulu@gmail.com>
2023-12-12 16:21:59 -0500

    Image & Headline chance

--- force-app/main/default/lwc/bonanzaCard/bonanzaCard.js
+++ force-app/main/default/lwc/bonanzaCard/bonanzaCard.js
@@ -1,10 +1,10 @@
 import { LightningElement, api } from "lwc";
 export default class BonanzaCard extends LightningElement {
-    @api title = 'Christmas Bonanza!';
+    /*@api title = 'Christmas Bonanza!';*/
 
     @api description = 'Offer 20% Coupon For Email & Address Update .';
 
-    pictureUrl = 'https://i.postimg.cc/L6T42RRG/target2-valeria-vinnik-246356.jpg';
+    pictureUrl = 'https://i.postimg.cc/2y1y3Kwn/Target-kristina-paukshtite-3444345.jpg';
 
     ready = false;
     connectedCallback() {
